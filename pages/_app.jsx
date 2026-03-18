fbd227b1f893280b3516e9e146affa3f6e507656
lalan <lalanikw@gmail.com>
2026-03-18 10:09:05 -1000

    adds

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,17 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6035916583698763"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
